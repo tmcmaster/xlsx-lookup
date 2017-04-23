3329638a0600e76fe92553a37845a2e62be6fe09
Tim McMaster <tim.mcmaster@iselect.com.au>
2017-04-23 14:16:32 +1000

    checkpoint

--- src/test/java/au/id/mcmaster/xlsxlookup/XLSXLookupApplicationTests.java
+++ src/test/java/au/id/mcmaster/xlsxlookup/XLSXLookupApplicationTests.java
@@ -1,4 +1,4 @@
-package au.id.mcmaster.wipdrools;
+package au.id.mcmaster.xlsxlookup;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class XLSXLookupApplicationTests {
 
 	@Test
 	public void contextLoads() {
