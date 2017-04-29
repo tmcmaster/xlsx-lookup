68121971838a64a8e21af58672ca8136d58c10a4
Tim McMaster <tim.mcmaster@iselect.com.au>
2017-04-29 14:50:48 +1000

    cleaning up the code a bit

--- src/main/java/au/id/mcmaster/xlsxlookup/XLSXLookupController.java
+++ src/main/java/au/id/mcmaster/xlsxlookup/XLSXLookupController.java
@@ -16,73 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class XLSXLookupController {
 	XLSXLookupService lookupService = new XLSXLookupService();
 	
-	@RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
-        return "greeting " + name;
-    }
-	
-	@RequestMapping(value = "/lookup/table", method = RequestMethod.GET)
+	@RequestMapping(value = "/table", method = RequestMethod.GET)
 	public Collection<String> table() {
 		return lookupService.getTableNames();
 	}
 	
-	@RequestMapping(value = "/lookup/table/{table}/options", method = RequestMethod.GET)
+	@RequestMapping(value = "/table/{table}/options", method = RequestMethod.GET)
 	public Map<String,List<String>> optionsMap(@PathVariable String table) {
 		return lookupService.getValueOptionsMap(table);
 	}
 	
-	@RequestMapping(value = "/lookup/table/{table}/fields", method = RequestMethod.GET)
+	@RequestMapping(value = "/table/{table}/fields", method = RequestMethod.GET)
 	public Collection<String> fields(@PathVariable String table) {
 		return lookupService.getFieldNames(table);
 	}
 	
-	@RequestMapping(value="/Test3", method=RequestMethod.POST)
-	public String lookup(@RequestBody Map<String,String> queryMap) {
-		return lookupService.getValue("Test3", queryMap);
-	}
-	
-	@RequestMapping(value="/table/{tableName}", method=RequestMethod.POST)
-	public String lookupTableData(@PathVariable String tableName, @RequestBody Map<String,String> queryMap) {
-		return lookupService.getValue(tableName, queryMap);
-	}
-	
-	@RequestMapping("/Test1")
-	public String lookup(@RequestParam String policyNumber, 
-						 @RequestParam String division,
-						 @RequestParam String category,
-						 @RequestParam String occupation, 
-						 @RequestParam String waitingPeriod,
-						 @RequestParam String benifitPeriod,
-						 @RequestParam String coverType,
-						 @RequestParam String policyPeriod,
-						 @RequestParam String age) {
-		
-		System.out.println(String.format(" policyNumber = %s", policyNumber));
-		System.out.println(String.format("     division = %s", division));
-		System.out.println(String.format("     category = %s", category));
-		System.out.println(String.format("   occupation = %s", occupation));
-		System.out.println(String.format("waitingPeriod = %s", waitingPeriod));
-		System.out.println(String.format("benifitPeriod = %s", benifitPeriod));
-		System.out.println(String.format("    coverType = %s", coverType));
-		System.out.println(String.format(" policyPeriod = %s", policyPeriod));
-		System.out.println(String.format("          age = %s", age));
-		
-		return lookupService.getValue("Test1", age, policyNumber, division, category, occupation, waitingPeriod, benifitPeriod, coverType, policyPeriod);
-	}
-	
-	@RequestMapping("/Test2")
-	public String lookup(@RequestParam String a, 
-						 @RequestParam String b,
-						 @RequestParam String c,
-						 @RequestParam String d,
-						 @RequestParam String r) {
-		
-		System.out.println(String.format("a = %s", a));
-		System.out.println(String.format("b = %s", b));
-		System.out.println(String.format("c = %s", c));
-		System.out.println(String.format("d = %s", d));
-		System.out.println(String.format("r = %s", r));
-		
-		return lookupService.getValue("Test2", r, a, b, c, d);
+	@RequestMapping(value="/table/{table}/value", method=RequestMethod.POST)
+	public String lookupTableData(@PathVariable String table, @RequestBody Map<String,String> queryMap) {
+		return lookupService.getValue(table, queryMap);
 	}
 }
--- src/test/java/au/id/mcmaster/xlsxlookup/XLSXLookupControllerTest.java
+++ src/test/java/au/id/mcmaster/xlsxlookup/XLSXLookupControllerTest.java
@@ -1,10 +1,26 @@
 package au.id.mcmaster.xlsxlookup;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class XLSXLookupControllerTest {
 	@Test
 	public void testCreate() {
 		new XLSXLookupController();
+	}
+	@Test
+	public void testGetValue() {
+		XLSXLookupController controller = new XLSXLookupController();
+		Map<String,String> queryMap = new HashMap<String,String>();
+		queryMap.put("ANB", "4");
+		queryMap.put("a", "BCD:1");
+		queryMap.put("b", "BC:2");
+		queryMap.put("c", "B:3");
+		queryMap.put("d", "BCDEF:4");
+		String value = controller.lookupTableData("Test 2", queryMap);
+		Assert.assertEquals("16", value);
 	}
 }
